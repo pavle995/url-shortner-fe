40cb3806076bb739df97a0ce9eaadea61f4afd29
pavle995 <kosuticp95@gmail.com>
2022-11-24 17:18:25 +0100

    changed hostname of api

--- pages/api/urlshortner.js
+++ pages/api/urlshortner.js
@@ -4,13 +4,13 @@ export default async function handler(req, res) {
         var input = JSON.parse(req.body)
         const payload = new URLSearchParams();
         payload.append('url', input.url)
-		const apiRes = await fetch(`http://ec2-15-161-149-189.eu-south-1.compute.amazonaws.com/short`,
+		const apiRes = await fetch(`https://urlshortnerapi.pavlekosutic.com/short`,
             {
                 method: 'POST',
                 body: payload
             });
 		const data = await apiRes.json();
-        const shortUrl = "http://ec2-15-161-149-189.eu-south-1.compute.amazonaws.com/".concat(data.shortUrl)
+        const shortUrl = "https://urlshortnerapi.pavlekosutic.com/".concat(data.shortUrl)
         console.log(shortUrl)
         res.status(200).json({url: shortUrl})
     } catch (err) {
